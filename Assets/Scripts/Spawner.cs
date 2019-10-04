ce1d52ba5c37ba59eacab2aa20297d066f29589e
Natalia Whiteley <nataliawhiteley11@gmail.com>
2019-10-04 14:24:09 -0400

    Added gold prefabs and methods to spawn gold to game manager.

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject SpawnNew(GameObject prefab, Vector2 pos)
+    {
+        GameObject obj = (GameObject)Instantiate(prefab);
+        obj.transform.position = pos;
+
+        return obj;
+    }
+}
